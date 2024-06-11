67cf173b838df3e8cd1b1a62114ff451d7bd26d3
SangHyeok-Kang <tdkgur200002@naver.com>
2024-06-11 15:05:48 +0900

    feat: 로그인 기능 구현 완료

--- src/router/index.js
+++ src/router/index.js
@@ -3,30 +3,35 @@ import Home from '../pages/Home.vue';
 import Profile from '../pages/Profile.vue';
 import Setting from '../pages/Setting.vue';
 import TransactionsCreate from '../pages/TransactionsCreate.vue'
-
+import Login from '../pages/login.vue'
 
 const routes = [
-    //가계부 등록 라우터
-    {
-      path: '/transactions/create',
-      name: 'TransactionsCreate',
-      component: TransactionsCreate
-    },
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path:'/profile',
-        name:'Profile',
-        component:Profile
-    },
-    {
-        path:'/settings',
-        name:'Setting',
-        component:Setting
-    },
+  //가계부 등록 라우터
+  {
+    path: '/transactions/create',
+    name: 'TransactionsCreate',
+    component: TransactionsCreate
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/settings',
+    name: 'Setting',
+    component: Setting
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
 ]
 
 const router = createRouter({
