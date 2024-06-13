3820eb53c5000f1fe785e4d0d2d63a115b9d0343
javastudyreview <rhwjdtlscjd123@gmail.com>
2024-06-13 10:20:25 +0900

    signup페이지 수정 완료

--- src/stores/UserRegistration.js
+++ src/stores/UserRegistration.js
@@ -1,38 +1,52 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
+import router from '../router'; 
 
 const url = "http://localhost:3001/users";
 
-export const useUsersStore = defineStore({
-  id: 'usersStore',
+export const useUserRegistrationStore = defineStore({
+  id: 'userRegistrationStore',
   state: () => ({
-    userId: "",
+
   }),
   actions: {
-    setUserId(userId) {
-      this.userId = userId;
-      console.log(this.userId);
-    },
-
-    async signup(userData) {
+    async registerUser(userData) {
       try {
         const response = await axios.post(url, userData);
-        return { success: true, message: '회원가입에 성공하였습니다.' };
+        alert('회원가입에 성공하였습니다.');
+        router.push('/login'); // 회원가입 성공 시 로그인 페이지로 이동함
       } catch (error) {
-        console.error('Error signing up:', error);
-        return { success: false, message: '회원가입에 실패하였습니다.' };
+        console.error('회원가입 오류:', error);
+        alert('회원가입에 실패하였습니다.');
       }
     },
 
-    logout() {
-      this.userId = "";
-    }
-  },
+    async checkUsername(username) {
+      try {
+        const response = await axios.get(`${url}?username=${username}`);
+        if (response.data.length === 0) {
+          alert('사용 가능한 아이디입니다.');
+        } else {
+          alert('이미 사용 중인 아이디입니다.');
+        }
+      } catch (error) {
+        console.error('아이디 중복 확인 오류:', error);
+        alert('아이디 중복 확인에 실패하였습니다.');
+      }
+    },
 
-  getters: {
-    getUserId() {
-      console.log(this.userId);
-      return this.userId;
-    }
-  }
+    async checkEmail(email) {
+      try {
+        const response = await axios.get(`${url}?email=${email}`);
+        if (response.data.length === 0) {
+          alert('사용 가능한 이메일입니다.');
+        } else {
+          alert('이미 사용 중인 이메일입니다.');
+        }
+      } catch (error) {
+        console.error('이메일 중복 확인 오류:', error);
+        alert('이메일 중복 확인에 실패하였습니다.');
+      }
+    },
+  },
 });
